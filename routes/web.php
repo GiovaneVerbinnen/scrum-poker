e2ef769688a57cc39d16f7fafd14535133524b13
Giovane Verbinnen de Oliveira <gioverbinnen@gmail.com>
2021-08-20 11:06:27 -0300

    add auth

--- routes/web.php
+++ routes/web.php
@@ -16,12 +16,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', Home::class)->name('home');
-Route::get('room', RoomPage::class)->name('room');
+Route::middleware(['auth'])->group(function () {
+    Route::get('/', Home::class)->name('home');
+    Route::get('room', RoomPage::class)->name('room');
 
-Route::get('room/{room}', function (Room $room) {
-    return view('room-join', compact('room'));
-})->name('room.join');
+    Route::get('room/{room}', function (Room $room) {
+        return view('room-join', compact('room'));
+    })->name('room.join');
+});
+
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
