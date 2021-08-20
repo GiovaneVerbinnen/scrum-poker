37995922b4e0b9813fc00c4213019fc7086cd307
Giovane Verbinnen de Oliveira <gioverbinnen@gmail.com>
2021-08-20 13:47:40 -0300

    some fixes

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,9 +6,12 @@
     </x-slot>
 
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="xs:max-w-xs max-w-xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <x-jet-welcome />
+                <a href="{{ route('home') }}"
+                    class="text-center text-2xl flex  bg-blue-500  space-x-2 px-3 py-2 rounded-lg text-white hover:bg-blue-600 focus:outline-none  xs:w-full justify-center">
+                    Room Page</a>
+                {{-- <x-jet-welcome /> --}}
             </div>
         </div>
     </div>
